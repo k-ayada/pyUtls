e276b0f769e0d52d181ce5f160fe5de7f42b9d0f
k-ayada <kiran14n@gmail.com>
2019-05-10 20:38:58 +0530

    Create __init__.py

---
+++ pyHelper/__init__.py
@@ -0,0 +1,9 @@
+#from .awsUtils.s3             import S3       as AwsS3
+#from .awsUtils.sts            import STS      as AwsSTS
+#from .awsUtils.ssm            import SSM      as AwsSSM
+#from .awsUtils.athena         import Athena   as AwsAthena
+#from .awsUtils.redshift       import Redshift as AwsRedshift
+#from .pySparkUtils.SparkUtils import SparkUtils
+
+name        = 'pyUtils'
+__version__ = '0.1'
